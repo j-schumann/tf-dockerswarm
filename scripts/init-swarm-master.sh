6d9aee458763318b065fc1e89f61824b5f19c480
j_schumann <114239+j-schumann@users.noreply.github.com>
2020-11-07 17:19:54 +0100

    add: adminer

--- scripts/init-swarm-master.sh
+++ scripts/init-swarm-master.sh
@@ -9,7 +9,14 @@
 parent_path=`dirname "$0"`
 env_file="$parent_path/../stacks/.env"
 
-cp $parent_path/../server-files/etc/sysctl.d/80-docker.conf /etc/sysctl.d/80-docker.conf
+# default directories for the container data
+mkdir -p /mnt/$GLUSTER_VOLUME/{traefik,mariadb/config,mariadb/db}
+
+# required for mariadb to start
+chown -R 1001:1001 /mnt/$GLUSTER_VOLUME/mariadb/db
+
+cp $parent_path/../server-files/etc/sysctl.d/80-docker.conf /etc/sysctl.d/
+cp $parent_path/../server-files/config/mariadb/my_custom.cnf /mnt/$GLUSTER_VOLUME/mariadb/config/
 
 # enp7s0 is specific to CPX servers, ens10 for CX servers
 export LOCALIP=`ip -o -4 addr show dev ens10 | cut -d' ' -f7 | cut -d'/' -f1`
@@ -24,12 +31,6 @@ docker swarm join-token worker -q > /mnt/$GLUSTER_VOLUME/join-token.txt
 
 # shared, encrypted mesh network for all containers on all nodes
 docker network create --opt encrypted --driver overlay traefik-net
-
-# default directories for the container data
-mkdir -p /mnt/$GLUSTER_VOLUME/{traefik,mariadb/config,mariadb/db}
-
-# required for mariadb to start
-chown -R 1001:1001 /mnt/$GLUSTER_VOLUME/mariadb/db
 
 sed -i \
     -e "s#PUBLIC_IP#$PUBLIC_IP#g" \
