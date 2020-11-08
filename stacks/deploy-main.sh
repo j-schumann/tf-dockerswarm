e08f06cbdca2e2afa2db3a4d61fece5e0fd18075
root <114239+j-schumann@users.noreply.github.com>
2020-11-08 21:17:52 +0100

    fix: deploy script callable from other dirs

--- stacks/deploy-main.sh
+++ stacks/deploy-main.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+parent_path=$(dirname "$0")
 # stack deploy does not support env-files, so prepare the config using docker-compose first...
-docker stack deploy main -c < $(docker-compose -f main.yaml --env-file .env config)
+docker stack deploy main -c < $(docker-compose -f $parent_path/main.yaml --env-file $parent_path/.env config)
